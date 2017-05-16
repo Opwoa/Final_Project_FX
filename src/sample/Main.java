50ec4cd222ab3e17f3b841e35ddc74a49fc50922
Opwoa <HornillaJ9651@sduhsd.lan>
2017-05-16 12:14:28 -0700

    started trying to do stuff

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,10 +3,13 @@
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.*;
+import javafx.geometry.Orientation;
 import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
 import javafx.stage.*;
 
 public class Main extends Application {
@@ -21,15 +24,14 @@ public class Main extends Application {
                 new ToggleButton("New"),
                 new Button("Open"),
                 new Button("Save"),
-                //new Separator(true),
+                new Separator(Orientation.VERTICAL),
                 new Button("Clean"),
                 new Button("Compile"),
                 new Button("Run"),
-                //new Separator(true),
+                new Separator(Orientation.VERTICAL),
                 new Button("Debug"),
                 new Button("Profile")
         );
-
         VBox layout = new VBox(10);
         layout.setStyle("-fx-background-color: cornsilk; -fx-padding: 20px;");
         layout.getChildren().setAll(
@@ -37,7 +39,7 @@ public class Main extends Application {
                 monitored,
                 reporter
         );
-        layout.setPrefWidth(800);
+        layout.setPrefWidth(600);
 
         stage.setScene(
                 new Scene(layout)
@@ -47,12 +49,14 @@ public class Main extends Application {
     }
 
     private Label createMonitoredLabel(final Label reporter) {
-        final Label monitored = new Label("     ");
+        final Label monitored = new Label();
 
-        monitored.setStyle("-fx-background-color: lightpink; -fx-text-fill: white; -fx-font-size: 400px;");
+        monitored.setPrefSize(560, 400);
+        monitored.setStyle("-fx-background-color: lightpink;");
 
         monitored.setOnMouseMoved(new EventHandler<MouseEvent>() {
-            @Override public void handle(MouseEvent event) {
+            @Override
+            public void handle(MouseEvent event) {
                 String msg =
                         "(x: "       + event.getX()      + ", y: "       + event.getY()       + ") -- " +
                                 "(sceneX: "  + event.getSceneX() + ", sceneY: "  + event.getSceneY()  + ") -- " +
@@ -65,6 +69,13 @@ public class Main extends Application {
         monitored.setOnMouseExited(new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent event) {
                 reporter.setText(OUTSIDE_TEXT);
+            }
+        });
+
+        monitored.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                Circle circle = new Circle(event.getX(), event.getX(), 5, Color.BLACK);
             }
         });
 
